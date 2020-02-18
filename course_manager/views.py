bd6f2159c7b9119d860caeec3508374dba09c79c
pushyami <pushyami@umich.edu>
2020-02-18 14:28:43 -0500

    Logging configurations

--- course_manager/views.py
+++ course_manager/views.py
@@ -5,9 +5,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView  # Import TemplateView
 import json
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-# Add the two views we have been talking about  all this time :)
 class HomePageView(TemplateView):
     template_name = "index.html"
 
@@ -16,14 +18,21 @@ def get_check_if_admin(request):
     """
    this API is checking if admin in the course
    """
+    logger.debug(get_check_if_admin.__name__)
     data = {'isAdmin': True}
-    print(data)
-    print(f"Session from course_id: {request.session['course_id']}")
+
+    logger.info(f"Session from course_id: {request.session['course_id']}")
     return HttpResponse(json.dumps(data))
 
 
 def admin_task(request):
+    """
+    This is called for performing admin related task like creating section, adding users via sis process
+    :param request:
+    :return:
+    """
+    logger.debug(admin_task.__name__)
     task = json.loads(request.body.decode("utf-8"))
-    print(task)
+    logger.info(f"Admin Task perfomed is {task}")
     course_id = request.session['course_id']
     return HttpResponse(json.dumps({'resp': True, 'course_id': course_id}))
